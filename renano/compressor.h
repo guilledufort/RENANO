fcaafe2770554ad2fa17f0cee0f2c14d0e2d06c0
Guillermo Dufort y Álvarez <gdufort@fing.edu.uy>
2021-04-02 14:30:15 -0300

    Added license message on files

--- renano/compressor.h
+++ renano/compressor.h
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2020 Guillermo Dufort y Álvarez
+// Copyright (c) 2021 Guillermo Dufort y Álvarez
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
